ad4f361e255500bb3b951bb174bdb116d409fda6
Gorka Revilla <gorka.revillafernandez@telefonica.com>
2019-05-26 18:51:53 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=setup_requirements,
     url='https://github.com/Ratxi/hoodex',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
