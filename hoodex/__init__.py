a827660e66aa6201e63b94ff484a9afbda270e47
Gorka Revilla <gorka.revillafernandez@telefonica.com>
2019-05-26 18:23:49 +0200

    Bump version: 0.1.0 → 0.1.1

--- hoodex/__init__.py
+++ hoodex/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Ratxi"""
 __email__ = 'ratxi.com@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=setup_requirements,
     url='https://github.com/Ratxi/hoodex',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
